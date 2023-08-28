631b5db7d444c907ae888d2c67fa97a037b20575
mdyasin111 <mdyasin7636@gmail.com>
2023-08-28 11:58:49 +0600

    dynamic button used

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,12 +1,15 @@
-import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import MyTasks from '../components/tasks/MyTasks';
-import TaskCard from '../components/tasks/TaskCard';
-import { useState } from 'react';
-import AddTaskModal from '../components/tasks/AddTaskModal';
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import MyTasks from "../components/tasks/MyTasks";
+import TaskCard from "../components/tasks/TaskCard";
+import { useState } from "react";
+import AddTaskModal from "../components/tasks/AddTaskModal";
+import { useSelector } from "react-redux";
 
 const Tasks = () => {
-
   const [isOpen, setIsOpen] = useState(false);
+  const { tasks } = useSelector((state) => state.tasksSlice);
+
+  console.log(tasks);
 
   return (
     <div className="h-screen grid grid-cols-12">
@@ -22,8 +25,13 @@ const Tasks = () => {
             <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
               <BellIcon className="h-6 w-6" />
             </button>
-            <button onClick={() => setIsOpen(!isOpen)} className="btn btn-primary">Add Task</button>
-            <AddTaskModal isOpen={isOpen} setIsOpen={setIsOpen}/>
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="btn btn-primary"
+            >
+              Add Task
+            </button>
+            <AddTaskModal isOpen={isOpen} setIsOpen={setIsOpen} />
             <div className="h-10 w-10 rounded-xl overflow-hidden">
               <img
                 src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
@@ -42,7 +50,9 @@ const Tasks = () => {
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
+              {tasks.map((item) => (
+                <TaskCard key={item.id} task={item} />
+              ))}
             </div>
           </div>
           <div className="relative h-[800px] overflow-auto">
@@ -53,19 +63,22 @@ const Tasks = () => {
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
-              <TaskCard />
+              {tasks.map((item) => (
+                <TaskCard key={item.id} task={item} />
+              ))}
             </div>
           </div>
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
-              <h1>Up Next</h1>
+              <h1>Complete</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
                 0
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
+              {tasks.map((item) => (
+                <TaskCard key={item.id} task={item} />
+              ))}
             </div>
           </div>
         </div>
